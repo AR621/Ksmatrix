8843c424ab066f8b36c1246954e5bcbbb0b326ad
root <root@localhost.localdomain>
2020-03-12 12:43:04 +0000

    Python alternative ver 1.0

--- Ksmatrix.py
+++ Ksmatrix.py
@@ -12,16 +12,36 @@ def arr_in():
             array[i].append(int(ext))
     return array
 
+def print_arr(array):
+    print("Input matrix:")
+    for i in range(0, len(array)):
+        print(array[i])
+
 def det(array):
     if len(array[0]) == 1:
-        det = array[0][0]
+        determinant = array[0][0]
     else:
-        det = "wip"
-    return det
+        #Laplace's expansion
+        determinant = 0
+        for i in range(0, len(array)):
+            #temporary smaller array
+            tmp = list()
+            for j in range(1, len(array)):
+                tmp.append(list())
+                for k in range(1, len(array)):
+                    if k - 1 < i:
+                        tmp[j - 1].append(array[j][k - 1])
+                    else:
+                        tmp[j - 1].append(array[j][k])
+        #Calculating determinant
+            if not i%2:
+                determinant = determinant + array[0][i] * det(tmp)
+            else:
+                determinant = determinant - array[0][i] * det(tmp)
+    return determinant
 
+#CODE
 ("Matrix determinant calculator.")
 array = arr_in()
-print("Input matrix:")
-for i in range(0, len(array)):
-    print(array[i])
+print_arr(array)
 print("Determinant of the following matrix is: " + str(det(array)))
